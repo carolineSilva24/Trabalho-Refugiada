e70f268eac0b9f5a40e7da8f6ed51076ca43422d
carolinesilva03 <102956881+carolineSilva24@users.noreply.github.com>
2022-05-25 16:37:51 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// Tag que permite ao ser clicado no botão ele mudar de tela
+function host(){
+  location.replace("Cadastro_host.html")
+}
+function refugiada(){
+  location.replace("Cadastro_refugiada.html")
+}
+function Entrar(){
+  location.replace("Login.html")
+}
+function back(){
+  location.replace("index.html")
+}
+function inicio(){
+  location.replace("index.html")
+}
+// Tag para colocar um alerta, se clicar em algum dos botões que tem função mas não tem uma pagina só para ele
+function entrar() {
+alert("A tela a seguir está em construção. Mas quando for feita, após confirmação dos dados, você seguirá para a tela do site em que você consiga ver suas informações.")
+  ;}
+function doar() {
+alert("A tela a seguir está em construção. Mas quando for feita irá aparecer uma tela informando sobre as formas de doação.")
+  ;}
+function sobre() {
+alert("A tela a seguir está em construção. Mas quando for feita irá aparecer uma tela informando sobre a ONG e seus projetos.")
+  ;}
+function documentação() {
+alert("A tela a seguir está em construção. Mas quando for feita irá aparecer uma tela informando sobre as dúvidas e perguntas mais frequentes.")
+  ;}
+function tradutor() {
+alert("A tela a seguir está em construção. Mas quando for feita irá aparecer uma aba para poder escolher em qual idioma você prefere para ler o site.")
+  ;}
+function cadastrar() {
+alert("A tela a seguir está em construção. Mas quando for feita, após confirmação dos dados, você seguirá para a tela do site em que você consiga ver suas informações.")
+  ;}
+function senha() {
+alert("A tela a seguir está em construção. Mas quando for feita, irá aparecer uma tela para você escrever seu email e recuperar/alterar a senha.")
+  ;}
+function continuar() {
+alert("A tela a seguir está em construção. Mas quando for feita, o site enviará um email para você pedindo que você altere sua senha por la.")
+  ;}
+// Tag que permite colocar um arquivo no cadastro da host e da refugiada 
+var inPerfil = document.getElementById("documento")
+var inPerfil = document.getElementById("comprovante")
+var inPerfil = document.getElementById("registro")
